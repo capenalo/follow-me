b040fa40b65fb3cebdcbd8e36c90b0ecad4db9a4
camila <penalozc@mail.gvsu.edu>
2016-04-16 22:09:07 +0000

    added static html for index, contact us and developers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'contact/index'
+
+  get 'developer/index'
+
+  get 'base/index'
+
   resources :paths
   resources :points
   resources :users
@@ -6,7 +12,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'base#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
